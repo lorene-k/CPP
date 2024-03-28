c3c70e76ac519cb9035f1b324c0a5333b05fef22
Lorene Khalifa <lkhalifa@bess-f3r2s1.clusters.42paris.fr>
2024-03-28 17:08:25 +0100

    DONE & CLEAN

---
+++ so_long_v5/src/clear.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clear.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkhalifa <lkhalifa@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/22 14:47:30 by lkhalifa          #+#    #+#             */
+/*   Updated: 2024/03/28 15:29:35 by lkhalifa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	clear_map(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	if (tab)
+		free(tab);
+	tab = 0;
+}
+
+int	clear_game(t_game *game)
+{
+	clear_map(game->map);
+	mlx_destroy_image(game->mlx, game->img.wall);
+	mlx_destroy_image(game->mlx, game->img.floor);
+	mlx_destroy_image(game->mlx, game->img.play);
+	mlx_destroy_image(game->mlx, game->img.exit);
+	mlx_destroy_image(game->mlx, game->img.coll);
+	mlx_destroy_image(game->mlx, game->img.enem);
+	mlx_destroy_image(game->mlx, game->img.exit_open);
+	mlx_destroy_window(game->mlx, game->win);
+	mlx_destroy_display(game->mlx);
+	free(game->mlx);
+	exit(0);
+	return (0);
+}
+
+void	clear_images(t_game *game)
+{
+	if (game->img.wall)
+		mlx_destroy_image(game->mlx, game->img.wall);
+	if (game->img.floor)
+		mlx_destroy_image(game->mlx, game->img.floor);
+	if (game->img.play)
+		mlx_destroy_image(game->mlx, game->img.play);
+	if (game->img.exit)
+		mlx_destroy_image(game->mlx, game->img.exit);
+	if (game->img.coll)
+		mlx_destroy_image(game->mlx, game->img.coll);
+	if (game->img.enem)
+		mlx_destroy_image(game->mlx, game->img.enem);
+	if (game->img.exit_open)
+		mlx_destroy_image(game->mlx, game->img.exit_open);
+	ft_printf("Error\nFile can't be opened.\n");
+	clear_map(game->map);
+	mlx_destroy_display(game->mlx);
+	free(game->mlx);
+	exit(1);
+}
---
+++ so_long_v5/src/so_long.h
@@ -0,0 +1,142 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkhalifa <lkhalifa@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/01 15:22:45 by lkhalifa          #+#    #+#             */
+/*   Updated: 2024/03/28 16:43:35 by lkhalifa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+
+# define SO_LONG_H
+
+# define WHITE 0xFFFFFF
+# define BLACK 0x000000
+# define RED 0xFF0000
+# define GREEN 0x00FF00
+
+# include "../mlx_linux/mlx.h"
+# include "libft/ft_printf/ft_printf.h"
+# include "libft/get_next_line/get_next_line.h"
+# include "libft/libft_src/libft.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+typedef struct s_lay
+{
+	int		row;
+	int		col;
+	int		exit;
+	int		coll;
+	int		play;
+}			t_lay;
+
+typedef struct s_check
+{
+	int		inv_char;
+	int		inv_walls;
+	int		inv_play;
+	int		inv_exit;
+	int		inv_coll;
+	int		inv_row;
+	int		last_line;
+}			t_check;
+
+typedef struct s_point
+{
+	int		x;
+	int		y;
+}			t_point;
+
+typedef struct s_img
+{
+	int		w;
+	int		h;
+	void	*wall;
+	void	*coll;
+	void	*play;
+	void	*exit;
+	void	*exit_open;
+	void	*floor;
+	void	*enem;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_img;
+
+typedef struct s_game
+{
+	void	*mlx;
+	void	*win;
+	t_img	img;
+	t_lay	*lay;
+	char	**map;
+	int		moves;
+	t_point	play_pos;
+}			t_game;
+
+/* CHECK MAP */
+void		check_layout(char *line, t_check *err_check, t_lay *lay,
+				int is_wall);
+void		get_line(char **line, char **p_line, char ***map_str);
+int			get_layout(int fd, t_check *err_check, t_lay *lay, char **map_str);
+char		**check_map(int fd, t_lay *lay);
+char		**check_args(int ac, char **av, t_lay *lay);
+
+/* CHECK PATH */
+char		**copy_map(char **map, t_point size);
+char		**flood_fill(char **map, t_point size, t_point cur);
+int			is_inv_exit(char **cpy, int i, int j);
+void		fill_path(char **map, t_point size, t_point start);
+void		check_path(char **map, t_lay *lay);
+
+/* CLEAR */
+void		clear_map(char **tab);
+int			clear_game(t_game *game);
+void		clear_images(t_game *game);
+
+/* HANDLE MOVES */
+void		move_up(t_game *game);
+void		move_down(t_game *game);
+void		move_left(t_game *game);
+void		move_right(t_game *game);
+int			on_keypress(int keysym, t_game *game);
+
+/* INIT GAME */
+void		display_img(t_game *g, int x, int y);
+int			render_map(t_game *game);
+void		display_all(t_game game);
+void		init_game(char **map, t_lay *lay);
+
+/* UTILS INIT */
+int			print_error(char *msg, char **map, char *str);
+t_lay		init_lay(void);
+t_check		init_check(void);
+void		*put_img(t_game *g, char *path);
+void		init_img(t_game *g);
+
+/* UPDATE */
+void		update_moves(t_game *game, t_point target_pos);
+void		update_pos(char *x, char *y, char a, char b);
+void		end_game(t_game *game, int outcome);
+void		update_game(t_game *game, t_point tp, int x, int y);
+
+/* UTILS CHECK */
+void		print_map_error(t_check err_check, char *map_str, t_lay *lay);
+void		check_last_line(char *line, t_check *err_check, t_lay *lay);
+int			ft_strcmp_sl(char *s1, char *s2);
+int			count_chars(char *str, char c);
+int			ft_strchr_sl(char *s, char c);
+
+#endif // SO_LONG_H
