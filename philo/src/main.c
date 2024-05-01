fdb6efc854ff8bae088ba3817b64a9fe2e7dbd34
Lorene Khalifa <lkhalifa@bess-f1r5s1.clusters.42paris.fr>
2024-05-01 13:08:15 +0200

    philo

---
+++ philo/src/main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkhalifa <lkhalifa@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 16:13:03 by lkhalifa          #+#    #+#             */
+/*   Updated: 2024/04/26 16:20:55 by lkhalifa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void    main(void)
+{
+    
+}
+
+all in ms :
+
+number_of_philosophers 
+time_to_die
+time_to_eat 
+time_to_sleep
+
+optional : number_of_times_each_philosopher_must_eat
---
+++ philo/src/philo.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkhalifa <lkhalifa@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/01 15:22:45 by lkhalifa          #+#    #+#             */
+/*   Updated: 2024/04/26 17:42:33 by lkhalifa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+
+# define PHILO_H
+
+# define FORK_TAKEN "has taken a fork"
+# define EATING "is eating"
+# define SLEEPING "is sleeping"
+# define THINKING "is thinking"
+# define DIED "died"
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+
+
+
+#endif // PHILO_H
