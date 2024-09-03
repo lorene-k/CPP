293d8f988438920fb779bbb774632899fcc0b743
Lorene <lorene@Freds-MacBook-Air.local>
2024-09-03 16:43:27 +0200

    cpp00/ex03 done

---
+++ cpp/cpp01/ex03/srcs/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkhalifa <lkhalifa@42.com>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/01 14:09:17 by lkhalifa          #+#    #+#             */
+/*   Updated: 2024/09/03 16:42:42 by lkhalifa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+    
+    std::cout << "Memory address of string variable: " << &str << std::endl;
+    std::cout << "Memory address of stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory address of stringREF: " << &stringREF << std::endl;
+
+    std::cout << "Value of string variable: " << str << std::endl;
+    std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value of stringREF: " << stringREF << std::endl;
+    return (0);
+}
