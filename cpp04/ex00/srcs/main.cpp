da0d4ca38abaf15bd2347ab39e006d1886e8b685
Lorene Khalifa <lkhalifa@made-f0Ar2s11.clusters.42paris.fr>
2024-12-12 12:46:42 +0100

    tests added for cpp04/ex00

--- cpp04/ex00/srcs/main.cpp
+++ cpp04/ex00/srcs/main.cpp
@@ -3,16 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkhalifa <lkhalifa@42.com>                 +#+  +:+       +#+        */
+/*   By: lkhalifa <lkhalifa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 16:10:29 by lkhalifa          #+#    #+#             */
-/*   Updated: 2024/12/09 20:07:46 by lkhalifa         ###   ########.fr       */
+/*   Updated: 2024/12/12 12:46:20 by lkhalifa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 #include "Dog.hpp"
 #include "WrongCat.hpp"
+
+/************************************************************ Functions tests */
+// int main()
+// {
+//     std::cout << CYAN << "===== Constructor Tests =====" << RESET << std::endl;
+//     Animal* animal = new Animal();
+//     Animal* dog = new Dog();
+//     Animal* cat = new Cat();
+//     WrongAnimal* wrong_animal = new WrongAnimal();
+//     WrongAnimal* wrong_cat = new WrongCat();
+    
+//     std::cout << std::endl << CYAN << "===== Type Tests =====" << RESET << std::endl;
+//     std::cout << "animal type = " << animal->getType() << " " << std::endl;
+//     std::cout << "dog type = " << dog->getType() << " " << std::endl;
+//     std::cout << "cat type = " << cat->getType() << " " << std::endl;
+//     std::cout << "wrong animal type = " << wrong_animal->getType() << " " << std::endl;
+//     std::cout << "wrong cat type = " << wrong_cat->getType() << " " << std::endl;
+    
+//     std::cout << std::endl << CYAN << "===== Sound Tests =====" << RESET << std::endl;
+//     std::cout << "animal sound = ";
+//     animal->makeSound();
+//     std::cout << "dog sound = ";
+//     dog->makeSound();
+//     std::cout << "cat sound = ";
+//     cat->makeSound();
+//     std::cout << "wrong animal sound = ";
+//     wrong_animal->makeSound();
+//     std::cout << "wrong cat sound = ";
+//     wrong_cat->makeSound();
+    
+//     std::cout << std::endl << CYAN << "===== Destructor Tests =====" << RESET << std::endl;
+//     delete animal;
+//     delete dog;
+//     delete cat;
+//     delete wrong_animal;
+//     delete wrong_cat;
+//     return (0);
+// }
 
 /********************************************************** WrongAnimal tests */
 // int main()
